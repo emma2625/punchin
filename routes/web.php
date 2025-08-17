2ecad06f3d57213b35ab48a5dca675e1f0a10926
emma2625 <emmanuelodobo10@gmail.com>
2025-08-17 18:32:45 +0100

    Added Branches

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => response()->json(status: 404));
+Route::get('mi', fn() => Artisan::call('migrate'));
