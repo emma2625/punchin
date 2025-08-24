40add2bb9b5175f85ccc17c0dc6633a9eccd9944
emma2625 <emmanuelodobo10@gmail.com>
2025-08-24 14:28:23 +0100

    Updated Subscription

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->domain('admin.punchin.test')
+            ->path('admin')
             ->login()
             ->profile(isSimple: false)
             ->favicon(asset('logo.png'))
